04b43ee134971b728c76253f0ad291a0891ef7ae
阿东 <konaod@gmail.com>
2017-06-04 07:46:22 +0800

    更新Token

--- FakeBear/Helpers/HttpClient.m
+++ FakeBear/Helpers/HttpClient.m
@@ -8,8 +8,8 @@
 
 #import "HttpClient.h"
 
-#define LU_TOKEN = @"remember_token=18621987299|03f9c5c9bfe5e6b31a1c0e968ba571d2672c6122"
-#define ME_TOKEN = @"remember_token=15626065551|fbc5817651db96b5809fab4dd77b899714dcb985"
+#define LU_TOKEN @"remember_token=18621987299|03f9c5c9bfe5e6b31a1c0e968ba571d2672c6122"
+#define ME_TOKEN @"remember_token=15626065551|fbc5817651db96b5809fab4dd77b899714dcb985"
 
 @interface HttpClient()
 
@@ -33,7 +33,7 @@
 {
     if (self = [super initWithBaseURL:url]) {
         [self.requestSerializer setValue:@"card/2.3.1 (iPhone; iOS 8.4.1; Scale/2.00)" forHTTPHeaderField:@"User-Agent"];
-        [self.requestSerializer setValue:@"remember_token=15626065551|fbc5817651db96b5809fab4dd77b899714dcb985" forHTTPHeaderField:@"Cookie"];
+        [self.requestSerializer setValue:ME_TOKEN forHTTPHeaderField:@"Cookie"];
     }
     return self;
 }
